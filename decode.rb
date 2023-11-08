ce8bab330f7e25c35c87952eaaa44d15cd858d4c
Enoh Collins <eno.col@gmail.com>
2023-11-08 13:15:38 +0000

    Add a method to decode morse character

---
+++ decode.rb
@@ -0,0 +1,17 @@
+def decode_char(morse_code)
+  morse_code_dict = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
+
+  morse_code_dict[morse_code] || 'Invalid Morse code character'
+end
+
+morse_char = ".-."
+decoded_char = decode_char(morse_char)
+puts decoded_char 
